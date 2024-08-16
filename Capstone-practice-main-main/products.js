303d00f2a269608f36c9fd58a306c13dfe92d379
Charles <charlesmabunga@gmail.com>
2024-08-16 21:13:12 +0800

    product data and start of forEach loop

---
+++ Capstone-practice-main-main/products.js
@@ -0,0 +1,58 @@
+const productsGrid = document.querySelector('#grid');
+
+const products = [
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  }, {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  }, {
+    image: 'images/image-placeholder.png',
+    name: 'Gaming Computer',
+    price: 20000
+  }
+];
+
+let productsHTML = '';
+
+products.forEach((product) => { 
+
+})
