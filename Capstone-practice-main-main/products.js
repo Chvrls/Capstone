fe2f7946d32122e1639b6854507c0c6085a9a5b8
Charles <charlesmabunga@gmail.com>
2024-08-16 21:46:42 +0800

    displaying the products html with javascript

--- Capstone-practice-main-main/products.js
+++ Capstone-practice-main-main/products.js
@@ -54,5 +54,14 @@ const products = [
 let productsHTML = '';
 
 products.forEach((product) => { 
-
+  productsHTML += `
+    <div class="product-card">
+    <div class="product-img-container">
+      <img class="product-image" src="${product.image}" alt="">
+    </div>
+      <p class="mt-3 mx-3 fw-bold">${product.name}</p>
+      <p class="mt-5 mx-3 fw-bold">₱${product.price}</p>
+    </div>`
 })
+
+productsGrid.innerHTML = productsHTML;
