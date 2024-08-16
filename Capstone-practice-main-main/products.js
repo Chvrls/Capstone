f73a77fdd314bf4528e297de4baeb0b81e6e98f8
Charles <charlesmabunga@gmail.com>
2024-08-16 23:04:42 +0800

    dom manipulation for motherboards

--- Capstone-practice-main-main/products.js
+++ Capstone-practice-main-main/products.js
@@ -2,12 +2,14 @@ import { pcCase } from "./temp-data-for-products/pcCase.js";
 import { monitors } from "./temp-data-for-products/monitors.js";
 import { cpu } from "./temp-data-for-products/cpu.js";
 import { gpu } from "./temp-data-for-products/gpu.js";
+import { motherboard } from "./temp-data-for-products/motherboard.js";
 
 const productsGrid = document.querySelector('#grid');
 const buttonMonitor = document.querySelector('#btn-monitor');
 const buttonPcCase = document.querySelector('#btn-pc-case');
 const buttonCPU = document.querySelector('#btn-cpu');
 const buttonGPU = document.querySelector('#btn-gpu');
+const buttonMotherboard = document.querySelector('#btn-motherboard');
 
 
 
@@ -69,6 +71,21 @@ buttonGPU.addEventListener('click', () => {
     </div>
       <p class="mt-3 mx-3 fw-bold">${gpu.name}</p>
       <p class="mt-5 mx-3 fw-bold">₱${gpu.price}</p>
+    </div>` 
+
+    productsGrid.innerHTML = productsHTML;
+  })
+});
+
+buttonMotherboard.addEventListener('click', () => { 
+  productsHTML = '';
+  motherboard.forEach((motherboard) => { 
+    productsHTML += `<div class="product-card">
+    <div class="product-img-container">
+      <img class="product-image" src="${motherboard.image}" alt="">
+    </div>
+      <p class="mt-3 mx-3 fw-bold">${motherboard.name}</p>
+      <p class="mt-5 mx-3 fw-bold">₱${motherboard.price}</p>
     </div>` 
 
     productsGrid.innerHTML = productsHTML;
---
+++ Capstone-practice-main-main/temp-data-for-products/motherboard.js
@@ -0,0 +1,50 @@
+export const motherboard = [
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  }, {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  },
+  {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  }, {
+    image: 'images/image-placeholder.png',
+    name: 'Gigabyte Z690 Aorus Pro',
+    price: 15000
+  }
+];
