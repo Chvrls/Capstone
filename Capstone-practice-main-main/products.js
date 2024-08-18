1bd9b506c8f5a66e80f10a86cefc70e96fb5c9f6
Charles <charlesmabunga@gmail.com>
2024-08-18 10:04:55 +0800

    start of eliminating the repetitiveness of codes

--- Capstone-practice-main-main/products.js
+++ Capstone-practice-main-main/products.js
@@ -21,124 +21,147 @@ const buttonPowerSupply = document.querySelector('#btn-power-supply');
 
 let productsHTML = '';
 
-buttonPcCase.addEventListener('click', () => {
-  productsHTML = '';
-  pcCase.forEach((pcCase) => {
-    productsHTML += `
-    <div class="product-card">
-    <div class="product-img-container">
-      <img class="product-image" src="${pcCase.image}" alt="">
-    </div>
-      <p class="mt-3 mx-3 fw-bold">${pcCase.name}</p>
-      <p class="mt-5 mx-3 fw-bold">₱${pcCase.price}</p>
-    </div>`
+// buttonPcCase.addEventListener('click', () => {
+//   productsHTML = '';
+//   pcCase.forEach((pcCase) => {
+//     productsHTML += `
+//     <div class="product-card">
+//     <div class="product-img-container">
+//       <img class="product-image" src="${pcCase.image}" alt="">
+//     </div>
+//       <p class="mt-3 mx-3 fw-bold">${pcCase.name}</p>
+//       <p class="mt-5 mx-3 fw-bold">₱${pcCase.price}</p>
+//     </div>`
 
-    productsGrid.innerHTML = productsHTML;
-  })
-});
+//     productsGrid.innerHTML = productsHTML;
+//   })
+// });
 
 
-buttonMonitor.addEventListener('click', () => { 
-  productsHTML = '';
-  monitors.forEach((monitor) => { 
-    productsHTML += `<div class="product-card">
-    <div class="product-img-container">
-      <img class="product-image" src="${monitor.image}" alt="">
-    </div>
-      <p class="mt-3 mx-3 fw-bold">${monitor.name}</p>
-      <p class="mt-5 mx-3 fw-bold">₱${monitor.price}</p>
-    </div>` 
+// buttonMonitor.addEventListener('click', () => {
+//   productsHTML = '';
+//   monitors.forEach((monitor) => {
+//     productsHTML += `<div class="product-card">
+//     <div class="product-img-container">
+//       <img class="product-image" src="${monitor.image}" alt="">
+//     </div>
+//       <p class="mt-3 mx-3 fw-bold">${monitor.name}</p>
+//       <p class="mt-5 mx-3 fw-bold">₱${monitor.price}</p>
+//     </div>`
 
-    productsGrid.innerHTML = productsHTML;
-  })
-});
+//     productsGrid.innerHTML = productsHTML;
+//   })
+// });
 
-buttonCPU.addEventListener('click', () => { 
-  productsHTML = '';
-  cpu.forEach((cpu) => { 
-    productsHTML += `<div class="product-card">
-    <div class="product-img-container">
-      <img class="product-image" src="${cpu.image}" alt="">
-    </div>
-      <p class="mt-3 mx-3 fw-bold">${cpu.name}</p>
-      <p class="mt-5 mx-3 fw-bold">₱${cpu.price}</p>
-    </div>` 
+// buttonCPU.addEventListener('click', () => {
+//   productsHTML = '';
+//   cpu.forEach((cpu) => {
+//     productsHTML += `<div class="product-card">
+//     <div class="product-img-container">
+//       <img class="product-image" src="${cpu.image}" alt="">
+//     </div>
+//       <p class="mt-3 mx-3 fw-bold">${cpu.name}</p>
+//       <p class="mt-5 mx-3 fw-bold">₱${cpu.price}</p>
+//     </div>`
 
-    productsGrid.innerHTML = productsHTML;
-  })
-});
+//     productsGrid.innerHTML = productsHTML;
+//   })
+// });
 
-buttonGPU.addEventListener('click', () => { 
-  productsHTML = '';
-  gpu.forEach((gpu) => { 
-    productsHTML += `<div class="product-card">
-    <div class="product-img-container">
-      <img class="product-image" src="${gpu.image}" alt="">
-    </div>
-      <p class="mt-3 mx-3 fw-bold">${gpu.name}</p>
-      <p class="mt-5 mx-3 fw-bold">₱${gpu.price}</p>
-    </div>` 
+// buttonGPU.addEventListener('click', () => {
+//   productsHTML = '';
+//   gpu.forEach((gpu) => {
+//     productsHTML += `<div class="product-card">
+//     <div class="product-img-container">
+//       <img class="product-image" src="${gpu.image}" alt="">
+//     </div>
+//       <p class="mt-3 mx-3 fw-bold">${gpu.name}</p>
+//       <p class="mt-5 mx-3 fw-bold">₱${gpu.price}</p>
+//     </div>`
 
-    productsGrid.innerHTML = productsHTML;
-  })
-});
+//     productsGrid.innerHTML = productsHTML;
+//   })
+// });
 
-buttonMotherboard.addEventListener('click', () => { 
-  productsHTML = '';
-  motherboard.forEach((motherboard) => { 
-    productsHTML += `<div class="product-card">
-    <div class="product-img-container">
-      <img class="product-image" src="${motherboard.image}" alt="">
-    </div>
-      <p class="mt-3 mx-3 fw-bold">${motherboard.name}</p>
-      <p class="mt-5 mx-3 fw-bold">₱${motherboard.price}</p>
-    </div>` 
+// buttonMotherboard.addEventListener('click', () => {
+//   productsHTML = '';
+//   motherboard.forEach((motherboard) => {
+//     productsHTML += `<div class="product-card">
+//     <div class="product-img-container">
+//       <img class="product-image" src="${motherboard.image}" alt="">
+//     </div>
+//       <p class="mt-3 mx-3 fw-bold">${motherboard.name}</p>
+//       <p class="mt-5 mx-3 fw-bold">₱${motherboard.price}</p>
+//     </div>`
 
-    productsGrid.innerHTML = productsHTML;
-  })
-});
+//     productsGrid.innerHTML = productsHTML;
+//   })
+// });
 
-buttonRAM.addEventListener('click', () => { 
-  productsHTML = '';
-  ram.forEach((ram) => { 
-    productsHTML += `<div class="product-card">
-    <div class="product-img-container">
-      <img class="product-image" src="${ram.image}" alt="">
-    </div>
-      <p class="mt-3 mx-3 fw-bold">${ram.name}</p>
-      <p class="mt-5 mx-3 fw-bold">₱${ram.price}</p>
-    </div>` 
+// buttonRAM.addEventListener('click', () => {
+//   productsHTML = '';
+//   ram.forEach((ram) => {
+//     productsHTML += `<div class="product-card">
+//     <div class="product-img-container">
+//       <img class="product-image" src="${ram.image}" alt="">
+//     </div>
+//       <p class="mt-3 mx-3 fw-bold">${ram.name}</p>
+//       <p class="mt-5 mx-3 fw-bold">₱${ram.price}</p>
+//     </div>`
 
-    productsGrid.innerHTML = productsHTML;
-  })
-});
+//     productsGrid.innerHTML = productsHTML;
+//   })
+// });
 
-buttonStorage.addEventListener('click', () => { 
-  productsHTML = '';
-  storage.forEach((storage) => { 
-    productsHTML += `<div class="product-card">
-    <div class="product-img-container">
-      <img class="product-image" src="${storage.image}" alt="">
-    </div>
-      <p class="mt-3 mx-3 fw-bold">${storage.name}</p>
-      <p class="mt-5 mx-3 fw-bold">₱${storage.price}</p>
-    </div>` 
+// buttonStorage.addEventListener('click', () => {
+//   productsHTML = '';
+//   storage.forEach((storage) => {
+//     productsHTML += `<div class="product-card">
+//     <div class="product-img-container">
+//       <img class="product-image" src="${storage.image}" alt="">
+//     </div>
+//       <p class="mt-3 mx-3 fw-bold">${storage.name}</p>
+//       <p class="mt-5 mx-3 fw-bold">₱${storage.price}</p>
+//     </div>`
 
-    productsGrid.innerHTML = productsHTML;
-  })
-});
+//     productsGrid.innerHTML = productsHTML;
+//   })
+// });
 
-buttonPowerSupply.addEventListener('click', () => { 
-  productsHTML = '';
-  powerSupply.forEach((powerSupply) => { 
-    productsHTML += `<div class="product-card">
-    <div class="product-img-container">
-      <img class="product-image" src="${powerSupply.image}" alt="">
-    </div>
-      <p class="mt-3 mx-3 fw-bold">${powerSupply.name}</p>
-      <p class="mt-5 mx-3 fw-bold">₱${powerSupply.price}</p>
-    </div>` 
+// buttonPowerSupply.addEventListener('click', () => {
+//   productsHTML = '';
+//   powerSupply.forEach((powerSupply) => {
+//     productsHTML += `<div class="product-card">
+//     <div class="product-img-container">
+//       <img class="product-image" src="${powerSupply.image}" alt="">
+//     </div>
+//       <p class="mt-3 mx-3 fw-bold">${powerSupply.name}</p>
+//       <p class="mt-5 mx-3 fw-bold">₱${powerSupply.price}</p>
+//     </div>`
 
-    productsGrid.innerHTML = productsHTML;
-  })
-});
+//     productsGrid.innerHTML = productsHTML;
+//   })
+// });
+
+function renderProductsHTML(category, product) { 
+  category.forEach((product) => {
+        productsHTML += `<div class="product-card">
+        <div class="product-img-container">
+          <img class="product-image" src="${product.image}" alt="">
+        </div>
+          <p class="mt-3 mx-3 fw-bold">${product.name}</p>
+          <p class="mt-5 mx-3 fw-bold">₱${product.price}</p>
+        </div>`
+    
+      productsGrid.innerHTML = productsHTML;
+    })
+}
+  
+buttonPowerSupply.addEventListener('click', () => { renderProductsHTML(powerSupply, powerSupply) });
+buttonPowerSupply.addEventListener('click', () => { renderProductsHTML(powerSupply, powerSupply) });
+buttonPowerSupply.addEventListener('click', () => { renderProductsHTML(powerSupply, powerSupply) });
+buttonPowerSupply.addEventListener('click', () => { renderProductsHTML(powerSupply, powerSupply) });
+buttonPowerSupply.addEventListener('click', () => { renderProductsHTML(powerSupply, powerSupply) });
+buttonPowerSupply.addEventListener('click', () => { renderProductsHTML(powerSupply, powerSupply) });
+buttonPowerSupply.addEventListener('click', () => { renderProductsHTML(powerSupply, powerSupply) });
+buttonPowerSupply.addEventListener('click', () => { renderProductsHTML(powerSupply, powerSupply) });
--- Capstone-practice-main-main/temp-data-for-products/powerSupply.js
+++ Capstone-practice-main-main/temp-data-for-products/powerSupply.js
@@ -12,7 +12,7 @@ export const powerSupply = [
   {
     image: 'images/image-placeholder.png',
     name: 'Corsair CX550F RGB',
-    price: 20000
+    price: 30000
   },
   {
     image: 'images/image-placeholder.png',
