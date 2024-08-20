48bc1e503dcf7e9bb9358f818c98ab3ac27b515e
Charles <charlesmabunga@gmail.com>
2024-08-20 11:17:50 +0800

    scrollable div

--- Capstone-practice-main-main/forge.js
+++ Capstone-practice-main-main/forge.js
@@ -1,4 +1,10 @@
 import { pcCase } from "./temp-data-for-products/pcCase.js";
+import { cpu } from "./temp-data-for-products/cpu.js";
+import { gpu } from "./temp-data-for-products/gpu.js";
+import { motherboard } from "./temp-data-for-products/motherboard.js";
+import { ram } from "./temp-data-for-products/ram.js";
+import { storage } from "./temp-data-for-products/storage.js";
+import { powerSupply } from "./temp-data-for-products/powerSupply.js";
 
 const caseBtn = document.querySelector('.case');
 const cpuBtn = document.querySelector('.cpu');
@@ -15,6 +21,9 @@ const showAllMotherboard = document.querySelector('.all-motherboard');
 const showAllRam = document.querySelector('.all-ram');
 const showAllStorage = document.querySelector('.all-storage');
 const showAllPowerSupply = document.querySelector('.all-power-supply');
+
+const componentCard = document.querySelector('#component-card');
+
 
 let productsHTML = '';
 
@@ -40,24 +49,34 @@ let productsHTML = '';
   
 // })
 
-function showChosenComponent() { 
-  showAllCase.classList.toggle('active');
-  if (showAllCase.classList.contains('active')) {
-      showAllCase.classList.remove('not-active');
-      pcCase.forEach((product) => {
+function showChosenComponent(componentDiv, componentName) { 
+  componentDiv.classList.toggle('show-components');
+  if (componentDiv.classList.contains('show-components')) {
+      componentDiv.classList.remove('hide-components');
+      componentName.forEach((product) => {
         productsHTML += `
-      <div class="shadow-none mx-3 bg-body-tertiary border border-dark-subtle border-bottom-0 fw-bold p-2" id="components-dropdown">
+      <div class="shadow-none mx-3 bg-body-tertiary border border-dark-subtle border-bottom-0 fw-bold p-1" id="components-dropdown">
         <img class="case-img me-5" src="${product.image}" alt="">
         <div class="component-info">
         <p class="component-info">${product.name}</p>
         </div>
         <div class="component-info text-end"><p class="text-end">+${product.price}</p></div>
       </div>`;
-        showAllCase.innerHTML = productsHTML;
+        componentDiv.innerHTML = productsHTML;
     });
-  } else if (!showAllCase.classList.contains('active')) {
-    showAllCase.classList.add('not-active');
+  } else if (!componentDiv.classList.contains('show-components')) {
+    componentDiv.classList.add('hide-components');
     productsHTML = '';
   }
 }
+
+caseBtn.addEventListener('click', () => { productsHTML = '';  showChosenComponent(showAllCase, pcCase) });
+cpuBtn.addEventListener('click', () => { productsHTML = ''; showChosenComponent(showAllCpu, cpu) });
+gpuBtn.addEventListener('click', () => { productsHTML = ''; showChosenComponent(showAllGpu, gpu) });
+motherboardBtn.addEventListener('click', () => { productsHTML = ''; showChosenComponent(showAllMotherboard, motherboard) });
+ramBtn.addEventListener('click', () => { productsHTML = ''; showChosenComponent(showAllRam, ram) });
+storageBtn.addEventListener('click', () => { productsHTML = ''; showChosenComponent(showAllStorage, storage) });
+powerSupplyBtn.addEventListener('click', () => { productsHTML = ''; showChosenComponent(showAllPowerSupply, powerSupply) });
+
+
 
