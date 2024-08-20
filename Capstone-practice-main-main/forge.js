66c8ed87ad61724ae03b9431db1804f702339bd0
Charles <charlesmabunga@gmail.com>
2024-08-20 09:43:44 +0800

    start of shortening the code

--- Capstone-practice-main-main/forge.js
+++ Capstone-practice-main-main/forge.js
@@ -7,11 +7,40 @@ const motherboardBtn = document.querySelector('.motherboard');
 const ramBtn = document.querySelector('.ram');
 const storageBtn = document.querySelector('.storage');
 const powerSupplyBtn = document.querySelector('.power-supply');
+
 const showAllCase = document.querySelector('.all-case');
+const showAllCpu = document.querySelector('.all-cpu');
+const showAllGpu = document.querySelector('.all-gpu');
+const showAllMotherboard = document.querySelector('.all-motherboard');
+const showAllRam = document.querySelector('.all-ram');
+const showAllStorage = document.querySelector('.all-storage');
+const showAllPowerSupply = document.querySelector('.all-power-supply');
 
 let productsHTML = '';
 
-caseBtn.addEventListener('click', () => {
+// caseBtn.addEventListener('click', () => {
+//   showAllCase.classList.toggle('active');
+//   if (showAllCase.classList.contains('active')) {
+//       showAllCase.classList.remove('not-active');
+//       pcCase.forEach((product) => {
+//         productsHTML += `
+//       <div class="shadow-none mx-3 bg-body-tertiary border border-dark-subtle border-bottom-0 fw-bold p-2" id="components-dropdown">
+//         <img class="case-img me-5" src="${product.image}" alt="">
+//         <div class="component-info">
+//         <p class="component-info">${product.name}</p>
+//         </div>
+//         <div class="component-info text-end"><p class="text-end">+${product.price}</p></div>
+//       </div>`;
+//         showAllCase.innerHTML = productsHTML;
+//     });
+//   } else if (!showAllCase.classList.contains('active')) {
+//     showAllCase.classList.add('not-active');
+//     productsHTML = '';
+//   }
+  
+// })
+
+function showChosenComponent() { 
   showAllCase.classList.toggle('active');
   if (showAllCase.classList.contains('active')) {
       showAllCase.classList.remove('not-active');
@@ -30,6 +59,5 @@ caseBtn.addEventListener('click', () => {
     showAllCase.classList.add('not-active');
     productsHTML = '';
   }
-  
-})
+}
 
