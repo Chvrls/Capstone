97774375ce2685dcb773faba5541ec9bc409383b
Charles <charlesmabunga@gmail.com>
2024-08-21 22:16:54 +0800

    login and signup

---
+++ login-signup.js
@@ -0,0 +1,43 @@
+const rightContent = document.querySelector('#right-content');
+const signUpBtn = document.querySelector('#signup-btn');
+const loginContent = rightContent.innerHTML;
+
+function renderSignUpPage() {
+  const renderSignUpHTML = `
+    <form action="/signup" class="d-flex flex-column align-items-center">
+      <div class="col-lg-10 mb-2">
+        <input required class="form-control border border-dark" type="text" name="username" id="username-input" placeholder="Username">
+      </div>
+      <div class="col-lg-10 mb-3">
+        <input required class="form-control border border-dark" type="email" name="email" id="email-input" placeholder="Email">
+      </div>
+      <div class="col-lg-10 mb-3">
+        <input required class="form-control border border-dark" type="password" name="password" id="password-input" placeholder="Password">
+      </div>
+      <div class="col-lg-10 mb-3">
+        <input required class="form-control border border-dark" type="password" name="confirm-password" id="confirm-password-input" placeholder="Confirm Password">
+      </div>
+      <button type="submit" class="btn btn-lg btn-outline-dark col-lg-10">Sign Up</button>
+    </form>
+    <div>
+      <p class="d-flex justify-content-center mt-5 mb-5 fw-bold">OR</p>
+    </div>
+    <div>
+      <p class="d-flex justify-content-center">If you already have an account, click here to login!</p>
+    </div>
+    <div class="d-flex justify-content-center mt-3">
+      <button class="btn btn-lg btn-outline-dark col-lg-10" id="login-btn">Login</button>
+    </div>`;
+
+  rightContent.innerHTML = renderSignUpHTML;
+}
+
+rightContent.addEventListener('click', (event) => {
+  if (event.target && event.target.id === 'signup-btn') {
+    renderSignUpPage();
+  } else if (event.target && event.target.id === 'login-btn') {
+    rightContent.innerHTML = loginContent;
+  }
+});
+
+signUpBtn.addEventListener('click', renderSignUpPage);
