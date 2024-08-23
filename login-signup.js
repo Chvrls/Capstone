b984fe7abc19ddb0179f1d0396471d5c4a480ceb
Charles <charlesmabunga@gmail.com>
2024-08-23 12:52:07 +0800

    start of form validation

--- login-signup.js
+++ login-signup.js
@@ -4,7 +4,7 @@ const loginContent = rightContent.innerHTML;
 
 function renderSignUpPage() {
   const renderSignUpHTML = `
-    <form action="/signup" class="d-flex flex-column align-items-center">
+    <form action="/signup" class="d-flex flex-column align-items-center" id="form">
       <div class="col-lg-10 mb-2">
         <input required class="form-control border border-dark" type="text" name="username" id="username-input" placeholder="Username">
       </div>
@@ -17,7 +17,7 @@ function renderSignUpPage() {
       <div class="col-lg-10 mb-3">
         <input required class="form-control border border-dark" type="password" name="confirm-password" id="confirm-password-input" placeholder="Confirm Password">
       </div>
-      <button type="submit" class="btn btn-lg btn-outline-dark col-lg-10">Sign Up</button>
+      <button type="submit" class="btn btn-lg btn-outline-dark col-lg-10 fw-bold">Sign Up</button>
     </form>
     <div>
       <p class="d-flex justify-content-center mt-5 mb-5 fw-bold">OR</p>
@@ -26,7 +26,7 @@ function renderSignUpPage() {
       <p class="d-flex justify-content-center">If you already have an account, click here to login!</p>
     </div>
     <div class="d-flex justify-content-center mt-3">
-      <button class="btn btn-lg btn-outline-dark col-lg-10" id="login-btn">Login</button>
+      <button class="btn btn-lg btn-outline-dark col-lg-10 fw-bold" id="login-btn">Login</button>
     </div>`;
 
   rightContent.innerHTML = renderSignUpHTML;
@@ -41,3 +41,11 @@ rightContent.addEventListener('click', (event) => {
 });
 
 signUpBtn.addEventListener('click', renderSignUpPage);
+
+//FORM VALIDATION
+
+const form = document.querySelector('#form');
+const userNameInput = document.querySelector('#username-input');
+const emailInput = document.querySelector('#email-input');
+const passwordInput = document.querySelector('#password-input');
+const confirmPasswordInput = document.querySelector('#confirm-password-input');
